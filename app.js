a307a0ac6df1f395ac938e8ae5a43e3c09d9fba8
freyako-ga <freyako96@gmail.com>
2024-06-13 16:06:58 +0100

    your message

---
+++ app.js
@@ -0,0 +1,48 @@
+const montage = {
+    gameFace :'🤩',
+    readyforThis : true,
+    victorySound : 'jazz',
+};
+
+console.log(montage.victorySound)
+
+const hometown = {
+    name : 'Freya',
+    population : 2,
+    country : {
+        name : 'Korea',
+        population : 3,
+    }
+}
+
+console.log(hometown.name, hometown.population)
+console.log(hometown.country.name, hometown.country.population)
+
+
+
+
+
+
+const movieCharacters = [
+    {
+        name: 'ella',
+        movie: 'ella enchanted'
+    }, {
+
+        name: 'freya',
+        movie: 'freya enchanted'
+    }, {
+        name: 'forest',
+        movie: 'forest gump'
+    }
+]
+    
+    console.log(movieCharacters[0])
+    
+    
+    
+const monkeyObject = {
+name: 'Freya',
+species: 'giraffe',
+const foodsEaten = [apple, banana]
+}
